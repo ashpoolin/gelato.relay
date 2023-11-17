d695940c1f041066f9ab3f5110badaceea4691e5
ashpool <ashpoolin@protonmail.com>
2023-11-16 19:31:34 -0700

    pretty decent

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-// const port = 3001;
 const port = 3000;
+// const port = 3001;
 
 var cors = require('cors');
 
-const webhook_server = require('./relay_server');
+const relay_server = require('./relay_server');
 
 app.use(cors());
 app.use(express.json());
 
 // app.post('/relay', (req, res) => {
-//   webhook_server.confirmAndSend(req)
+//   relay_server.confirmAndSend(req)
 //   .then(response => {
 //     res.status(200).send(response);
 //   })
@@ -28,7 +28,7 @@ app.post('/relay', (req, res) => {
   let retries = 0;
 
   const sendRequest = () => {
-    webhook_server.confirmAndSend(req)
+    relay_server.confirmAndSend(req)
       .then(response => {
         res.status(200).send(response);
       })
